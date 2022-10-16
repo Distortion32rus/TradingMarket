680af87e325e5e47774bddea09b0cb3f10e70dca
Aptline <d.a.bspl@gmail.com>
2022-10-16 20:57:03 +0300

    версия без RestController в прайсе

--- src/main/java/ru/bspl/pet/tradingmarket/controllers/PriceListController.java
+++ src/main/java/ru/bspl/pet/tradingmarket/controllers/PriceListController.java
@@ -4,6 +4,7 @@ package ru.bspl.pet.tradingmarket.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ import java.sql.Timestamp;
 import java.util.List;
 
 
-@RestController
+@Controller
 @RequestMapping("/pricelist")
 public class PriceListController {
 
